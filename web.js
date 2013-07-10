ead35bea72779759851d4a5faa15beb42f5d95d2
vagrant <vagrant@precise32.(none)>
2013-07-10 16:22:04 +0000

    web.js: fix buffer encoding problem on heroku

--- web.js
+++ web.js
@@ -2,21 +2,21 @@ var express = require('express');
 var fs = require('fs');
 require('buffer');
 
-var file = fs.readFileSync('./index.html', {encoding: 'utf8'}, function(err, data) {
-	if (err) throw err;
-	console.log(data);
-});
+// var file = fs.readFileSync('./index.html', {encoding: 'utf8'}, function(err, data) {
+// 	if (err) throw err;
+// 	console.log(data);
+// });
 
-console.log(file);
+/* console.log(file); */
 
-var buffer = new Buffer(file, "utf8");
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf8');
 
 console.log(buffer);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf8'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
