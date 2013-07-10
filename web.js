fb754de647851495f41b59868ef6064371a72413
vagrant <vagrant@precise32.(none)>
2013-07-10 15:40:27 +0000

    Add buffer to web.js

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var file = fs.readFileSync('index.html', function(err, data) {
+	if (err) throw err;
+		console.log(data);
+});
+
+var buffer = new Buffer(file, "utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
